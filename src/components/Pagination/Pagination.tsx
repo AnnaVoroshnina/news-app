022a68cc70f0df20b307bf9f95365207775a9d48
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-08-30 14:26:24 +0300

    add pagination, change api

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css'
+
+export const Pagination = ({totalPages, handleNextPage, handlePrevPage, handlePageClick, currentPage}) => {
+    return (
+        <div className={styles.pagination}>
+            <button
+                className={styles.arrow}
+                onClick={handlePrevPage}
+                disabled={currentPage <=1}
+            >{'<'}</button>
+            <div className={styles.list}>
+                {[...Array(totalPages)].map((_, index) => {
+                    return <button
+                        className={styles.pageNumber}
+                        key={index}
+                        onClick={() => handlePageClick(index + 1)}
+                        disabled={index + 1 === currentPage}
+                    >{index + 1}</button>
+                })}
+            </div>
+            <button
+                className={styles.arrow}
+                onClick={handleNextPage}
+                disabled={currentPage >= totalPages}
+
+            >{'>'}</button>
+        </div>
+    )
+}
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,34 +4,69 @@ import {useEffect, useState} from "react";
 import {getNews} from "../../api/apiNews.ts";
 import {NewsList} from "../../components/NewsList/NewsList.tsx";
 import {Skeleton} from "../../components/Skeleton/Skeleton.tsx";
+import {Pagination} from "../../components/Pagination/Pagination.tsx";
 
 export const Main = () =>{
     //создаем состояние
     const [news, setNews] = useState<NewsBanner[]>([]);
     //состояние для отображения скелетона
     const [isLoading, setIsLoading] = useState(true);
+    //состояние текущей страницы
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 10
+    const pageSize = 10
 
-    //будем делать запрос к серверу
-    useEffect(() => {
-        setIsLoading(true)
-        const fetchNews = async () =>{
-            try {
-                const response = await getNews()
-                setNews(response.news)
-                setIsLoading(false)
-            }
-            catch (error){
-                console.log(error);
-            }
+    //запрос к серверу
+    const fetchNews = async (currentPage) =>{
+        try {
+            setIsLoading(true)
+            const response = await getNews(currentPage, pageSize)
+            setNews(response.news)
+            setIsLoading(false)
         }
-        //не забываем вызвать функцию
-        fetchNews()
-    }, []);
-    console.log(news)
+        catch (error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchNews(currentPage)
+    }, [currentPage]);
+
+//навигация по кнопкам
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+
+        }
+    }
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
+
     return (
         <main className={styles.main}>
             {news.length > 0 && !isLoading ? (<NewsBanner item={news[0]}/>) : (<Skeleton type={'banner'} count={1} />)}
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePrevPage={handlePrevPage}
+                handlePageClick={handlePageClick}
+                currentPage={currentPage}
+            />
             {!isLoading ? (<NewsList news={news}/>) : (<Skeleton type={'item'} count={10} />)}
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePrevPage={handlePrevPage}
+                handlePageClick={handlePageClick}
+                currentPage={currentPage}
+            />
         </main>
 )
 }
