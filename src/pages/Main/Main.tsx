fdac71b9185815ef759458ce5ad9d4a764bc9c41
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-08-30 11:15:39 +0300

    add skeleton for news and banner

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,17 +3,22 @@ import {NewsBanner} from "../../components/NewsBanner/newsBanner.tsx";
 import {useEffect, useState} from "react";
 import {getNews} from "../../api/apiNews.ts";
 import {NewsList} from "../../components/NewsList/NewsList.tsx";
+import {Skeleton} from "../../components/Skeleton/Skeleton.tsx";
 
 export const Main = () =>{
     //создаем состояние
     const [news, setNews] = useState<NewsBanner[]>([]);
+    //состояние для отображения скелетона
+    const [isLoading, setIsLoading] = useState(true);
 
     //будем делать запрос к серверу
     useEffect(() => {
+        setIsLoading(true)
         const fetchNews = async () =>{
             try {
                 const response = await getNews()
                 setNews(response.news)
+                setIsLoading(false)
             }
             catch (error){
                 console.log(error);
@@ -25,9 +30,8 @@ export const Main = () =>{
     console.log(news)
     return (
         <main className={styles.main}>
-            {news.length > 0 ? <NewsBanner item={news[0]}/> : null}
-
-            <NewsList news={news} />
+            {news.length > 0 && !isLoading ? (<NewsBanner item={news[0]}/>) : (<Skeleton type={'banner'} count={1} />)}
+            {!isLoading ? (<NewsList news={news}/>) : (<Skeleton type={'item'} count={10} />)}
         </main>
 )
 }
