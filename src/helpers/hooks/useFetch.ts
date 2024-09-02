0d28a8c7836805fcd93f4cf3ca49f4aee8883201
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-09-02 14:41:06 +0300

    refactoring

---
+++ src/helpers/hooks/useFetch.ts
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+
+export const useFetch = (fetchFunction, params) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    //преобразовываем зараметры из массива в строку, чтобы легче было сравнивать в зависимости у useEffect
+    const stringParams = params ? new URLSearchParams(params).toString() : ''
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true)
+                const result = await fetchFunction(params)
+                setData(result)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        })()
+
+    }, [fetchFunction, stringParams]);
+    return {data, isLoading, error}
+}
