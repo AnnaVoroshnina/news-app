09e48732d8383a5b66d5f435dead722a1385cb54
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-09-02 14:41:06 +0300

    refactoring

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -3,12 +3,17 @@ import styles from './styles.module.css'
 export const Categories = ({categories, setSelectedCategory, selectedCategory}) =>{
     return (
         <div className={styles.categories}>
+            <button
+                className={!selectedCategory ? styles.active : styles.item}
+                onClick={() => setSelectedCategory(null)}
+            >All
+            </button>
             {categories.map(category => {
                 return (
                     <button
                         key={category}
                         className={selectedCategory === category ? styles.active : styles.item}
-                        onClick={()=>setSelectedCategory(category)}
+                        onClick={() => setSelectedCategory(category)}
                     >{category}</button>
                 )
             })}
