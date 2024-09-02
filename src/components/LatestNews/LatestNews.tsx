2a920f49ebef77e3f497acd2a6dd04cf0993a3ec
AnnaVoroshnina <bologova.ann@yandex.ru>
2024-09-02 17:28:46 +0300

    add latest news

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,11 +1,14 @@
 
 import styles from './styles.module.css'
 import {BannersList} from "../BannersList/BannersList.tsx";
+import {useFetch} from "../../helpers/hooks/useFetch.ts";
+import {getLatestNews} from "../../api/apiNews.ts";
 
-export const LatestNews = ({banners}) =>{
+export const LatestNews = () =>{
+    const {data} = useFetch(getLatestNews)
     return (
         <section className={styles.section}>
-            <BannersList banners={banners}/>
+            <BannersList banners={data && data.news} />
         </section>
     )
 }
